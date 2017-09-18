d6996fc6320ad1c8cb324995c2aa95abae81ef62
SenpaiWeb <easycode@tuta.io>
2017-09-19 00:07:39 +0200

    new features
    password is now hidden as echo except if you use --show
    it also has a new interactive feature using "py generator.py -e"

--- generator.py
+++ generator.py
@@ -8,13 +8,18 @@ import re
 save = True # do not touch
 savepath = os.getcwd() + "\precious.info" # save path
 length = 32 # length of the generated password
+Show = False # do not touch
 if(len(sys.argv)>=2):
     label = sys.argv[1]
     for cnt,arg in enumerate(sys.argv):
         if arg == "--purge-output":
             open(savepath, "w").write("")
             exit()
-        if arg == "--not-save":
+        elif arg == "-e":
+            label = input("Label: ")
+        elif arg == "--show":
+            Show = True
+        elif arg == "--not-save":
             save = False
         elif arg == "-l":
             try:
@@ -22,17 +27,18 @@ if(len(sys.argv)>=2):
             except:
                 print("Could not understand length, set to: "+str(length))
 else:
-    print("error while executing, correct usage is following: py "+sys.argv[0]+" <label> [-l <length>] [--not-save]")
+    print("error while executing, correct usage is following: py "+sys.argv[0]+" <label or -e> [-l <length>] [--not-save] [--show]")
     exit()
 with open(savepath, "a+") as passesFile:
     passesFile.seek(0,0)
     readFile = passesFile.read()
     if((label + ":") not in readFile):
         urand = str(os.urandom(length)).replace("<", "!")
-        md5 = hashlib.md5(urand.encode("utf-8")).hexdigest()[:length]
+        password = hashlib.md5(urand.encode("utf-8")).hexdigest()[:length]
         if save:
-            passesFile.write("\n"+label+":"+md5)
+            passesFile.write(label+":"+password+"\n")
     else:
-        md5 = re.search(label+":(.+)",readFile).group(1)
-os.system("echo "+md5+"|clip")
-print("Set to clipboard! ["+md5+"]")
+        password = re.search(label+":(.+)",readFile).group(1)
+os.system("echo "+password+"|clip")
+if Show:
+    print(password)
